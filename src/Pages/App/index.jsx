f1e98f8aed21eeb078fdc5b9ea69932b28c8ad9d
mijail <mijail86@gmail.com>
2024-08-15 13:52:57 -0500

    feat: pages, routes and navbar

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,32 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "../Home";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import MyAccount from "../MyAccount";
+import NotFound from "../NotFound";
+import SingIn from "../Signin";
+import NavBar from "../../Components/NavBar/index";
+import "./App.css";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/sign-in", element: <SingIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+  return routes;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+      <NavBar />
+    </BrowserRouter>
+  );
+};
+
+export default App;
