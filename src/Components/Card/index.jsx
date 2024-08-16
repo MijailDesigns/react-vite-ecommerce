15b83320f19209e1350be695e67ab61d85451d3f
mijail <mijail86@gmail.com>
2024-08-15 23:04:30 -0500

    feat: fetching data

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,13 +1,13 @@
-const Card = () => {
+const Card = ({ data }) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs p-1 m-2">
-          Electronis
+          {data?.category}
         </span>
         <img
-          src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="headphones"
+          src={data?.image}
+          alt={data?.title}
           className="w-full h-full object-cover rounded-lg"
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full p-1 m-2">
@@ -15,8 +15,8 @@ const Card = () => {
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+        <span className="text-sm font-light truncate mr-2">{data?.title}</span>
+        <span className="text-lg font-medium">${data?.price}</span>
       </p>
     </div>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 
 function Home() {
+  const apiUrl = "https://fakestoreapi.com/products";
+  // const apiUrl2 = "https://api.escuelajs.co/api/v1/products";
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(`${apiUrl}`)
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <Layout>
       Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
       <Card />
     </Layout>
   );
